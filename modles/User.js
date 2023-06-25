d5294290674ccd853a4a291fc6c13c5259baaf39
NRS08 <nirantravindra20bcs138@iiitkottayam.ac.in>
2023-06-26 02:10:41 +0530

    wallet connect

--- modles/User.js
+++ modles/User.js
@@ -31,6 +31,10 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please provide password"],
     minlength: 6,
   },
+  account: {
+    type: String,
+    required: [true, "Please connect to wallet"],
+  },
 });
 
 userSchema.pre("save", async function (next) {
